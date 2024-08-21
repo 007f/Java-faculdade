48940fc65f0ced7b10404b3b71440ea3e0256494
007f <88677725+007f@users.noreply.github.com>
2024-08-21 19:01:56 -0300

    Update Pessoa.java

--- Aula02/src/main/java/org/example/Pessoa.java
+++ Aula02/src/main/java/org/example/Pessoa.java
@@ -1,16 +1,21 @@
 package org.example;
 
 public class Pessoa {
+    /*uma Classe é apresentada como uma abertura de estrutura classe
+    seus atributos podem ser apresentados como publico privado ou apenas o tipo e nome referente.
+    como mostrado abaixo*/
     //atributos
     public String nome;
     public String endereco;
-    public int idade;
+    private int idade;
 
+    /*um construtor é uma função que recebe os dados do main ou outra classe e cria um objeto colocando os valores
+    nas */
     //construtor
     public Pessoa(){
 
     }
-
+    //construtor
     public Pessoa(String p1, String p2){
         this.nome = p1;
         this.endereco = p2;
