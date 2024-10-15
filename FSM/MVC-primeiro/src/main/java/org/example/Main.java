9fbc2cf0d8e536b671b21456999607c1bef20c2a
007f <88677725+007f@users.noreply.github.com>
2024-10-14 22:25:54 -0300

    introdução a modelo-visão e controle

---
+++ FSM/MVC-primeiro/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+import org.example.view.UsuarioView;
+
+public class Main {
+    public static void main(String[] args) {
+        UsuarioView usuarioView = new UsuarioView();
+
+        usuarioView.cadastrarUsuario();
+
+    }
+}
